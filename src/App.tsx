c7587d6eee0d3d8c60dcf123f66430b6c1a263a9
ty <composedkim@naver.com>
2025-04-30 14:20:49 +0900

    ✨feat: Part 2 컴포넌트 UI 생성
    ChannelBox, DropSort, MemberBox,SearchPost 컴포넌트 생성
    a.txt 삭제 및 lucide-react install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import ChannelBox from './components/ChannelBox';
+import DropSort from './components/DropSort';
+import MembeerBox from './components/MemberBox';
+import SearchPost from './components/SearchPost';
+
 export default function App() {
   return (
-    <>
-      <h1>App Component</h1>
-    </>
+    <div className='flex bg-black h-[100vh] justify-center align-middle gap-3 pt-16'>
+      <MembeerBox />
+      <ChannelBox />
+      <DropSort />
+      <SearchPost />
+    </div>
   );
 }
