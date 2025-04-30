4d5951ee07c06544b73e82075de376905a2eecd9
Chosojeong <chosojeong97@gmail.com>
2025-04-30 15:45:31 +0900

    app.tsx 오류

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import Header from "./components/header/Header";
-import Button from "./components/common/Button";
-import Input from "./components/common/Input";
-
 export default function App() {
   return <></>;
 }
