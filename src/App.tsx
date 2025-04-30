2897ee67ca513ab413b7c2c9a2017892779b655b
sduddla <sduddla921@gmail.com>
2025-04-30 17:27:52 +0900

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Avatar from './components/avatar/Avatar';
+import PostBtn from './components/btn/PostBtn';
+import VoteBtn from './components/btn/VoteBtn';
+import ChannelName from './components/channel/ChannelName';
+import ChannelBox from './components/sidebar/ChannelBox';
+import Button from './components/common/Button';
+import Input from './components/common/Input';
+import DropSort from './components/post/DropSort';
+import Header from './components/header/Header';
+import BoldIcon from './components/icon/BoldIcon';
+import CodeEditIcon from './components/icon/CodeEditIcon';
+import ImageIcon from './components/icon/ImageIcon';
+import MembeerBox from './components/sidebar/MemberBox';
+import Notification from './components/notification/Notification';
+import LikeComment from './components/reaction/LikeComment';
+import SearchPost from './components/post/SearchPost';
+
 export default function App() {
-  import Avatar from "./components/avatar/Avatar";
-  import ChannelName from "./components/channel/ChannelName";
-  import LikeComment from "./components/reaction/LikeComment";
   return (
     <>
       <Avatar
@@ -14,6 +28,20 @@ export default function App() {
         title="선택의 갈림길에서 함께 답을 찾는 채널"
       />
       <LikeComment likeCount={12} commentCount={10} />
+      <PostBtn />
+      <VoteBtn />
+      <Button value="LogIn" className="button-style1" />
+      <Input type="email" placeholder="Email" className="input-style1" />
+
+      <Header />
+      <BoldIcon />
+      <CodeEditIcon />
+      <ImageIcon />
+      <Notification />
+      <ChannelBox />
+      <DropSort />
+      <MembeerBox />
+      <SearchPost />
     </>
   );
 }
