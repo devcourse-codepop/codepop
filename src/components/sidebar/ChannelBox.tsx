e607a5bab08f2b3a047f0d798c9ea1ff3167d170
ty <composedkim@naver.com>
2025-05-08 12:42:36 +0900

    🎨Design: 멤버 박스 margin 설정

--- src/components/sidebar/ChannelBox.tsx
+++ src/components/sidebar/ChannelBox.tsx
@@ -1,8 +1,8 @@
 export default function ChannelBox() {
   return (
     <>
-      <div className='w-[291px] h-[211px] bg-white rounded-[10px] shadow-md font-semibold'>
-        <h2 className='text-[20px] font-medium text-[#595956] ml-7 mt-5 mb-[14px]'>Channel</h2>
+      <div className='w-[291px] h-[211px] bg-white rounded-[10px] shadow-md font-semibold mb-[30px]'>
+        <h2 className='text-[20px] font-medium text-[#595956] ml-7 pt-5 mb-[14px]'>Channel</h2>
         <ul className='space-y-[13px]'>
           <li className='flex items-start ml-[29px]'>
             <span className='w-1 h-8 bg-[#10215C] rounded-sm mr-[7px]'></span>
