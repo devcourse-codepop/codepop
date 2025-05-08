a3e0cd38ec8db047c248b43021947cb3fd1653ff
yubin121 <yubin045@naver.com>
2025-05-08 11:24:28 +0900

    ✨ Feat: Zustand를 이용하여 usePostStore 생성

---
+++ src/store/postStore.ts
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+export const usePostStore = create(() => ({
+  channelIdList: [
+    '681b84d4437f722b6908ab61',
+    '681b850d437f722b6908ab65',
+    '681b8570437f722b6908ab69',
+  ],
+}));
