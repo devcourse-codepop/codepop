316a0dc56809f209ee699933b6ab74184ad78dbc
yubin121 <yubin045@naver.com>
2025-05-08 10:54:03 +0900

    ✨ Feat: 날짜 포맷을 위한 dayjs 설치 및 props 수정

--- src/components/post/PostListItem.tsx
+++ src/components/post/PostListItem.tsx
@@ -1,43 +1,58 @@
-import Avatar from "../avatar/Avatar";
-import LikeComment from "../reaction/LikeComment";
+import Avatar from '../avatar/Avatar';
+import LikeComment from '../reaction/LikeComment';
+import { Post } from '../../types';
+import dayjs from 'dayjs';
+import { useState } from 'react';
 
-export default function PostListItem({
-  title,
-  updatedAt,
-}: {
-  title: { title: string; content: string };
-  updatedAt: string;
-}) {
-  // const getDatetimeFormat = () => {
-  //   const date = dayjs(updatedAt);
-  //   return date.format("YYYY.MM.DD");
-  // };
+export default function PostListItem(props: Post) {
+  const { _id, title, image, author, likes, comments, updatedAt } = props;
+
+  const [isLiking, setIsLiking] = useState(false);
+  const clickLikeHandler = () => {
+    setIsLiking(!isLiking);
+  };
+
+  const getDatetimeFormat = () => {
+    const date = dayjs(updatedAt);
+    return date.format('YYYY.MM.DD');
+  };
 
   return (
     <>
-      <div className="w-full h-auto rounded-[5px] bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-        <div className="h-[85px] pl-3 pt-2.5">
+      <div className="w-full h-auto rounded-[5px] bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] cursor-pointer relative">
+        <div className="flex justify-between h-[85px] pl-3 pt-2.5">
           <Avatar
-            name="사용자"
-            email="user123@gmail.com"
-            image="../src/assets/images/avatar.svg"
+            name={author.fullName}
+            email={author.email}
+            image={author.image}
           />
         </div>
         <div className="flex flex-col">
           <div className="pt-[19px] px-[55px] pb-[12px] text-[20px] font-semibold">
-            {title.title}
+            {JSON.parse(title).title}
           </div>
           <div className="pt-[11px] px-[55px] pb-[23px] text-[15px] font-normal">
-            {title.content}
+            {JSON.parse(title).content}
           </div>
+          {image && (
+            <div className="px-[55px] pb-[23px]">
+              <img src={image} />
+            </div>
+          )}
           <div className="flex justify-end pr-5 pb-[9px] text-[#808080] text-sm font-light">
-            {updatedAt}
-            {/* {getDatetimeFormat()} */}
+            {getDatetimeFormat()}
           </div>
         </div>
         <hr className="mx-[18px] text-[#b2b2b2]" />
         <div className="h-[59px]">
-          <LikeComment likeCount={12} commentCount={10} />
+          <LikeComment
+            likeCount={likes.length}
+            commentCount={comments.length}
+            postId={_id}
+            isLiking={isLiking}
+            clickLikeHandler={clickLikeHandler}
+            likes={likes}
+          />
         </div>
       </div>
     </>
