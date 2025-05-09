6c8f106ae84821f6382a3d53d44b6bc0ebc200b8
Chosojeong <chosojeong97@gmail.com>
2025-05-09 09:24:42 +0900

    🐛 Fix : Link 태그로 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,8 @@
-import alarm from "../../assets/images/header/alarm.svg";
+import { Link } from "react-router-dom";
 import logo from "../../assets/images/header/logo.svg";
 import user from "../../assets/images/header/userImg.svg";
+import Notification from "../notification/Notification";
+import { useState } from "react";
 
 export default function Header() {
   const auth = true;
@@ -9,24 +11,26 @@ export default function Header() {
     <>
       <header className="h-[100px] px-[60px] flex items-center justify-between">
         <h1>
-          <a href="/">
+          <Link to="/">
             <img src={logo} />
-          </a>
+          </Link>
         </h1>
         <div>
           {!auth && <a href="">Login</a>}
           {auth && (
             <div className="flex items-center gap-6">
-              <a href="" className="text-[20px]">
+              <Link to="/logout" className="text-[20px]">
                 Logout
-              </a>
-              <a href="" className="relative">
-                <img src={alarm} />
-                <span className="block w-[8px] h-[8px] rounded-2xl bg-[#FF0000] absolute right-0 top-0.5"></span>
-              </a>
-              <a href="" className="w-10 h-10 rounded-3xl overflow-hidden">
+              </Link>
+              <div className="notification-wrapper">
+                <Notification></Notification>
+              </div>
+              <Link
+                to="/mypage"
+                className="w-10 h-10 rounded-3xl overflow-hidden"
+              >
                 <img src={user} />
-              </a>
+              </Link>
             </div>
           )}
         </div>
