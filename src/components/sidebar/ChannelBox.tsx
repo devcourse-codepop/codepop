55cdec31d1c3f017a8375dff75b502f25388be73
Chosojeong <chosojeong97@gmail.com>
2025-05-09 10:44:43 +0900

    🐛 Fix : 메뉴 배열로 정리, 클릭한 메뉴 활성화 되도록 수정

--- src/components/sidebar/ChannelBox.tsx
+++ src/components/sidebar/ChannelBox.tsx
@@ -1,4 +1,16 @@
+import { Link, useLocation } from "react-router-dom";
+
 export default function ChannelBox() {
+  const pathName = useLocation().pathname;
+
+  const menuItems = [
+    { name: "MysteryCode", to: "/channel/MysteryCode", color: "#10215C" },
+    { name: "DeskSetup", to: "/channel/DeskSetup", color: "#3380DE" },
+    { name: "Vote", to: "/channel/Vote", color: "#60A7F7" },
+  ];
+
+  // console.log(pathName.split("/channel")[1]);
+
   return (
     <>
       <div className="w-[291px] h-[211px] bg-white rounded-[10px] pt-5 shadow-md font-semibold">
@@ -6,18 +18,28 @@ export default function ChannelBox() {
           Channel
         </h2>
         <ul className="space-y-[13px]">
-          <li className="flex items-start ml-[29px]">
-            <span className="w-1 h-8 bg-[#10215C] rounded-sm mr-[7px]"></span>
-            <span className="font-noto font-[18px] pt-1">이거 왜 되지?</span>
-          </li>
-          <li className="flex items-start ml-[29px]">
-            <span className="w-1 h-8 bg-[#3380DE] rounded-sm mr-[7px]"></span>
-            <span className="font-[18px] pt-1">이거 왜 안 쓰지?</span>
-          </li>
-          <li className="flex items-start ml-[29px] ">
-            <span className="w-1 h-8 bg-[#60A7F7] rounded-sm mr-[7px]"></span>
-            <span className="font-[18px] pt-1">골라봐</span>
-          </li>
+          {menuItems.map((item) => (
+            <li>
+              <Link
+                // onClick={() => clickHandler(pathName)}
+                to={item.to}
+                className="flex items-start ml-[29px] group"
+              >
+                <span
+                  className={`w-1 h-8 bg-[${item.color}] rounded-sm mr-[7px]`}
+                ></span>
+                <span className="font-noto font-[18px] pt-1 relative z-1">
+                  이거 왜 되지?
+                  <span
+                    className={`block w-0 h-3/7 bg-[${item.color}] opacity-30 absolute left-0 bottom-0 -z-1 group-hover:w-full duration-300 ease-out`}
+                    style={{
+                      width: pathName === item.to ? "100%" : "",
+                    }}
+                  ></span>
+                </span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </>
