4f5cae61457c7a0ee2c005f20c524bf565ad6be7
yubin121 <yubin045@naver.com>
2025-05-12 22:10:23 +0900

    🐛 Fix: 헤더 프로필 이미지 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,35 +5,35 @@ import { useAuthStore } from '../../stores/authStore';
 
 export default function Header() {
   const { isLoggedIn, user, logout } = useAuthStore();
-  console.log(user?.coverImage);
+  console.log(user?.image);
   let imgSrc: string = '';
-  if (user?.coverImage === undefined || user?.coverImage === '') {
+  if (user?.image === undefined || user?.image === '') {
     imgSrc =
       'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg';
   } else {
-    imgSrc = user?.coverImage;
+    imgSrc = user?.image;
   }
   return (
     <>
-      <header className='h-[100px] px-[60px] flex items-center justify-between'>
+      <header className="h-[100px] px-[60px] flex items-center justify-between">
         <h1>
-          <Link to='/'>
+          <Link to="/">
             <img src={logo} />
           </Link>
         </h1>
         <div>
-          {!isLoggedIn && <Link to='/login'>Login</Link>}
+          {!isLoggedIn && <Link to="/login">Login</Link>}
           {isLoggedIn && (
-            <div className='flex items-center gap-6'>
-              <Link to='/' onClick={logout} className='text-[20px]'>
+            <div className="flex items-center gap-6">
+              <Link to="/" onClick={logout} className="text-[20px]">
                 Logout
               </Link>
-              <div className='notification-wrapper'>
+              <div className="notification-wrapper">
                 <Notification />
               </div>
               <img
                 src={imgSrc}
-                className='w-10 h-10 rounded-3xl overflow-hidden cursor-pointer'
+                className="w-10 h-10 rounded-3xl overflow-hidden cursor-pointer"
                 onClick={() => (window.location.href = '/profile')}
               />
             </div>
