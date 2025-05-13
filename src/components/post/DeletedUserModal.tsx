8aa7d7c93b4b41df1227711a4de055b99a02518f
yubin121 <yubin045@naver.com>
2025-05-13 12:10:39 +0900

    ✨ Feat: 탈퇴한 회원 게시글 모달창 구현

---
+++ src/components/post/DeletedUserModal.tsx
@@ -0,0 +1,39 @@
+import Button from '../common/Button';
+import close from '../../assets/images/closeBtn.svg';
+
+export default function DeletedUserModal({
+  closeUserModalHanlder,
+}: {
+  closeUserModalHanlder: () => void;
+}) {
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] flex justify-center items-center z-[1000]">
+        <div className="bg-white p-5 rounded-[5px] text-center w-[300px] flex flex-col gap-10">
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between">
+              <div className="opacity-70 text-[13px]">Notice</div>
+              <img
+                src={close}
+                alt="close"
+                className="w-4 h-4 cursor-pointer"
+                onClick={closeUserModalHanlder}
+              />
+            </div>
+            <hr className="opacity-30" />
+          </div>
+          <div className="opacity-80 text-[15px]">
+            탈퇴한 회원의 게시글입니다!
+          </div>
+          <div className="flex justify-end mr-5">
+            <Button
+              className="button-style3 text-[12px]"
+              value="확인"
+              onClick={closeUserModalHanlder}
+            ></Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
