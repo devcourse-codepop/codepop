5135962878b93099efef57acdc41cef95c8c3335
sduddla <sduddla921@gmail.com>
2025-05-13 20:03:03 +0900

    ✨ Feat: DarkMode.tsx

--- src/components/toggle/DarkMode.tsx
+++ src/components/toggle/DarkMode.tsx
@@ -16,7 +16,7 @@ export default function DarkMode({ children }: DarkModeProps) {
   const themes: Theme[] = [
     {
       name: 'Light',
-      bg: 'bg-gradient-to-r from-white to-[#c0cfe7]',
+      bg: 'bg-gradient-to-b from-white to-[#c0cfe7]',
       logo: logoLight,
     },
     {
@@ -26,6 +26,7 @@ export default function DarkMode({ children }: DarkModeProps) {
     },
   ];
 
+  // 테마 불러오기
   const getTheme = () => {
     const storeTheme = localStorage.getItem('themeIndex');
     return storeTheme ? Number(storeTheme) : 0;
@@ -33,19 +34,22 @@ export default function DarkMode({ children }: DarkModeProps) {
 
   const [themeIndex, setThemeIndex] = useState(getTheme);
 
+  // themeIndex 바뀔 떄마다 localSotrage에 저장해주기
   useEffect(() => {
     localStorage.setItem('themeIndex', String(themeIndex));
   }, [themeIndex]);
 
+  // 사용할 테마 정보
   const theme = themes[themeIndex];
 
+  // dark, light 전환
   const nextTheme = () => {
     setThemeIndex((currentTheme) => (currentTheme + 1) % themes.length);
   };
 
   return (
     <div
-      className={`min-h-screen ${theme.bg} transition-colors duration-300 flex flex-col relative`}
+      className={`min-h-screen ${theme.bg} transition-colors duration-300 flex flex-col`}
     >
       <main>{children(theme, nextTheme)}</main>
     </div>
