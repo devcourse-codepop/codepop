7c70131f35aa0ea0bd3c0f5530259099cedccd3b
ty <composedkim@naver.com>
2025-05-14 16:51:08 +0900

    ✨Feat: 메시지 모달 닫을 때 store에서 닫기

--- src/pages/message/ChatModal.tsx
+++ src/pages/message/ChatModal.tsx
@@ -1,27 +1,32 @@
 import { useState, useEffect } from 'react';
 import ChatRoom from './ChatRoom';
 import ChatUserList from './ChatUserList';
+import { useMessageStore } from '../../stores/messageStore';
+import MessageOpenIcon from '../../assets/images/messageOpenIcon.svg';
 
 interface ChatModalProps {
   isOpen: boolean;
   onClose: () => void;
+  initialUser?: User1 | null;
 }
 
-export interface User {
-  id: string;
+export interface User1 {
+  id?: string;
   name: string;
   content?: string;
   count?: number;
 }
 
-export default function ChatModal({ isOpen, onClose }: ChatModalProps) {
-  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+export default function ChatModal({ isOpen, onClose, initialUser }: ChatModalProps) {
+  const [selectedUser, setSelectedUser] = useState<User1 | null>(null);
+  const changeMessageIcon = useMessageStore((state) => state.setMessageIcon);
 
   useEffect(() => {
     if (isOpen) {
-      setSelectedUser(null);
+      setSelectedUser(initialUser || null);
+      changeMessageIcon(MessageOpenIcon);
     }
-  }, [isOpen]);
+  }, [isOpen, initialUser]);
 
   if (!isOpen) return null;
 
@@ -29,9 +34,9 @@ export default function ChatModal({ isOpen, onClose }: ChatModalProps) {
     <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50' onClick={onClose}>
       <div className='w-[448px] h-[75vh] bg-white rounded-[5px] flex flex-col' onClick={(e) => e.stopPropagation()}>
         {selectedUser ? (
-          <ChatRoom user={selectedUser} onBack={() => setSelectedUser(null)} onClose={onClose} />
+          <ChatRoom user={selectedUser} onBack={() => setSelectedUser(null)} onClose={onClose} /> // 채팅방
         ) : (
-          <ChatUserList onSelectUser={setSelectedUser} onClose={onClose} />
+          <ChatUserList onSelectUser={setSelectedUser} onClose={onClose} /> // 대화했던 사람들 목록
         )}
       </div>
     </div>
