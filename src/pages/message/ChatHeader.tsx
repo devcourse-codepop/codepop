4c8819a485fbc60a7a8f08290efe0ffc2d37320c
sduddla <sduddla921@gmail.com>
2025-05-14 19:38:22 +0900

    🔄️ 메시지 기능, 이미지 폴더 정리

--- src/pages/message/ChatHeader.tsx
+++ src/pages/message/ChatHeader.tsx
@@ -1,5 +1,5 @@
-import backBtn from '../../assets/images/arrow-back-outline.svg';
-import closeBtn from '../../assets/images/close-outline.svg';
+import backBtn from '../../assets/images/message/arrow-back-outline.svg';
+import closeBtn from '../../assets/images/message/close-outline.svg';
 
 interface ChatHeaderProps {
   userName?: string;
@@ -7,18 +7,32 @@ interface ChatHeaderProps {
   onClose: () => void;
 }
 
-export default function ChatHeader({ userName, onBack, onClose }: ChatHeaderProps) {
+export default function ChatHeader({
+  userName,
+  onBack,
+  onClose,
+}: ChatHeaderProps) {
   return (
-    <div className='flex items-center justify-between p-[20px] border-b border-b-[#DEDEDE] bg-white rounded-t-[5px]'>
+    <div className="flex items-center justify-between p-[20px] border-b border-b-[#DEDEDE] bg-white rounded-t-[5px]">
       {onBack ? (
-        <img src={backBtn} onClick={onBack} className=' w-[24px] h-[24px]  mr-2 cursor-pointer' />
+        <img
+          src={backBtn}
+          onClick={onBack}
+          className=" w-[24px] h-[24px]  mr-2 cursor-pointer"
+        />
       ) : (
-        <div className='w-4' /> // 공간 맞춤용
+        <div className="w-4" /> // 공간 맞춤용
       )}
 
-      <p className='text-[20px] font-medium flex-1 text-center'>{userName || 'Message'}</p>
+      <p className="text-[20px] font-medium flex-1 text-center">
+        {userName || 'Message'}
+      </p>
 
-      <img src={closeBtn} onClick={onClose} className=' w-[24px] h-[24px] ml-2 cursor-pointer' />
+      <img
+        src={closeBtn}
+        onClick={onClose}
+        className=" w-[24px] h-[24px] ml-2 cursor-pointer"
+      />
     </div>
   );
 }
