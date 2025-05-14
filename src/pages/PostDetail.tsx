f07e93971a10841b1ddf61531168db1398d9889f
yubin121 <yubin045@naver.com>
2025-05-14 15:46:13 +0900

    ✨ Feat: 댓글 아이콘 클릭 시, 상세 페이지의 댓글 작성 컴포넌트로 스크롤 이동하도록 구현

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -1,10 +1,10 @@
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import ChannelName from '../components/channel/ChannelName';
 import PostDetailItem from '../components/post/PostDetailItem';
 import WriteCommentItem from '../components/post/WriteCommentItem';
 import { getPostList } from '../api/post/post';
 import { usePostStore } from '../stores/postStore';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Post } from '../types';
 
 export default function PostDetail() {
@@ -12,19 +12,38 @@ export default function PostDetail() {
   const channel = params.channelId;
   const post = params.postId;
 
+  const location = useLocation();
+
+  // 채널 id 값 받아오기
   const channelIdList = usePostStore((state) => state.channelIdList);
 
+  // 댓글 작성 컴포넌트를 나타내는 div 요소
+  const commentRef = useRef<HTMLDivElement | null>(null);
+
+  // 게시글 상태
   const [postItem, setPostItem] = useState<Post | null>(null);
 
+  // 댓글 작성할 때마다 useEffect를 재실행하기 위한 트리거
+  const [reloadTrigger, setReloadTrigger] = useState(0);
+  const updateReloadTrigger = () => {
+    setReloadTrigger((reloadTrigger) => reloadTrigger + 1);
+  };
+
+  // 해당 게시글만 필터링
   const filteringItem = (data: Post[]) => {
+    console.log('filteringItem');
     for (const res of data) {
       if (res._id === post) {
-        setPostItem(res);
+        setPostItem(structuredClone(res));
+        console.log('setPostItem');
+        console.log('reloadTrigger :', reloadTrigger);
       }
     }
   };
 
+  // 게시글 목록 불러오기 (게시글 id에 해당하는 게시글만 필터링)
   const getPostItem = async () => {
+    console.log('getPostItem');
     try {
       const { data } = await getPostList(channelIdList[Number(channel) - 1]);
       filteringItem(data);
@@ -35,24 +54,44 @@ export default function PostDetail() {
 
   useEffect(() => {
     getPostItem();
-  }, []);
+  }, [reloadTrigger]);
+
+  // 댓글 아이콘 클릭 후 현재 페이지 접근 시, 댓글 작성 컴포넌트가 화면에 보이도록 스크롤 조정
+  useEffect(() => {
+    if (location.state?.scrollToComment && postItem) {
+      const timeout = setTimeout(() => {
+        if (commentRef.current) {
+          commentRef.current.scrollIntoView({
+            block: 'center',
+            behavior: 'smooth',
+          });
+        }
+      }, 500);
+      return () => clearTimeout(timeout);
+    }
+  }, [location.state, commentRef, postItem]);
 
   return (
     <>
-      {/* ml-[50px] */}
       <div className="w-full">
         <div className="flex justify-between items-end pb-[30px]">
           <ChannelName channelId={String(channel)} />
         </div>
-        {/* max-h-[605px] */}
         {postItem && (
           <div className="flex flex-col gap-[30px] max-h-[calc(100vh-100px-120px)] overflow-auto">
-            <PostDetailItem key={postItem?._id} {...postItem} />
-            <WriteCommentItem
-              channelId={String(channel)}
-              postId={String(post)}
-              postUserId={postItem.author._id}
+            {/* <PostDetailItem key={postItem?._id} {...postItem} /> */}
+            <PostDetailItem
+              {...postItem}
+              updateReloadTrigger={updateReloadTrigger}
             />
+            <div ref={commentRef}>
+              <WriteCommentItem
+                channelId={String(channel)}
+                postId={String(post)}
+                postUserId={postItem.author._id}
+                updateReloadTrigger={updateReloadTrigger}
+              />
+            </div>
           </div>
         )}
       </div>
