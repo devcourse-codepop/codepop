e961d868a506f288d694915fcdd8fc3e85a19b0b
Chosojeong <chosojeong97@gmail.com>
2025-05-15 23:48:06 +0900

    🐛 Fix : api 요청 Interval 2초에서 10초로 수정

--- src/components/sidebar/MemberBox.tsx
+++ src/components/sidebar/MemberBox.tsx
@@ -41,7 +41,7 @@ export default function MemberBox({ theme }: { theme: Theme }) {
   useEffect(() => {
     fetchUsers();
 
-    const interval = setInterval(fetchUsers, 2000);
+    const interval = setInterval(fetchUsers, 10000);
 
     return () => clearInterval(interval);
   }, []);
@@ -92,14 +92,14 @@ export default function MemberBox({ theme }: { theme: Theme }) {
       >
         Member
       </h2>
-      <div className="flex items-center text-[#898FA3] bg-[#F6F8FA] px-3 py-2 rounded-[5.54px] text-[14px] gap-4 mb-[13px]">
+      <div className='flex items-center text-[#898FA3] bg-[#F6F8FA] px-3 py-2 rounded-[5.54px] text-[14px] gap-4 mb-[13px]'>
         <div>
-          <Search className="w-[19.94px] h-[19.94px] text-[#86879C]" />
+          <Search className='w-[19.94px] h-[19.94px] text-[#86879C]' />
         </div>
         <input
-          type="text"
-          placeholder="멤버를 검색해 보세요"
-          className=" outline-none placeholder:text-[#898FA3] placeholder:text-[14px]w-full"
+          type='text'
+          placeholder='멤버를 검색해 보세요'
+          className=' outline-none placeholder:text-[#898FA3] placeholder:text-[14px]w-full'
           onChange={(e) => searchHandler(e)}
         />
       </div>
@@ -126,9 +126,9 @@ export default function MemberBox({ theme }: { theme: Theme }) {
       >
         {/* 유저멤버 카드 */}
         {filterUsers.map((user) => (
-          <div className="relative" key={user._id} id={user._id}>
+          <div className='relative' key={user._id} id={user._id}>
             <div
-              className="memberCard cursor-pointer"
+              className='memberCard cursor-pointer'
               onClick={() => ToggleHandelr(user._id)}
             >
               <Avatar
@@ -142,12 +142,12 @@ export default function MemberBox({ theme }: { theme: Theme }) {
 
             {/* 프로필 클릭시 나오는 modal */}
             <button
-              className="absolute right-0 top-4 cursor-pointer"
+              className='absolute right-0 top-4 cursor-pointer'
               onClick={() => ToggleHandelr(user._id)}
             >
               <img
                 src={dark(theme) ? menuIconWhite : menuIcon}
-                className="rotate-90"
+                className='rotate-90'
               />
               {openUser === user._id && (
                 <ul
@@ -160,7 +160,7 @@ export default function MemberBox({ theme }: { theme: Theme }) {
                 >
                   <li>
                     <Link
-                      className="px-3 py-1 block opacity-70 hover:opacity-100"
+                      className='px-3 py-1 block opacity-70 hover:opacity-100'
                       to={`/profile`}
                       state={{ userid: user._id }}
                     >
@@ -168,7 +168,7 @@ export default function MemberBox({ theme }: { theme: Theme }) {
                     </Link>
                   </li>
                   <li
-                    className="px-3 py-1 block  opacity-70 hover:opacity-100"
+                    className='px-3 py-1 block  opacity-70 hover:opacity-100'
                     onClick={() => {
                       setChatTargetUser({ id: user._id, name: user.fullName });
                       setIsChatOpen(true);
