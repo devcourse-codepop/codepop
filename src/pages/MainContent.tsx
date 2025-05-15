f890bfd0d80cc6c1fd38e7fbbe00e3d5667c1bf3
Chosojeong <chosojeong97@gmail.com>
2025-05-15 12:22:18 +0900

    🚚 Move : main-content.css import 위치 변경

--- src/pages/MainContent.tsx
+++ src/pages/MainContent.tsx
@@ -1,9 +1,10 @@
-import Banner from "../components/main-content/Banner";
-import PopularPost from "../components/main-content/PopularPost";
+import Banner from '../components/main-content/Banner';
+import PopularPost from '../components/main-content/PopularPost';
+import './../css/main-content/main-content.css';
 
 export default function MainContent() {
   return (
-    <div className="w-full h-full overflow-y-auto scroll-custom">
+    <div className='w-full h-full overflow-y-auto scroll-custom'>
       <Banner />
       <PopularPost />
     </div>
