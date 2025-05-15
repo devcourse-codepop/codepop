e6cba0528c007dc37addb1ad4779ec1acc47aaf5
ty <composedkim@naver.com>
2025-05-15 15:56:22 +0900

    ✨Feat: 언팔로우  api 추가

--- src/api/follow/follow.ts
+++ src/api/follow/follow.ts
@@ -3,3 +3,9 @@ import { axiosInstance } from '../axios';
 export const postFollow = (userId: string) => {
   return axiosInstance.post<Follow>('/follow/create', { userId });
 };
+
+export const postUnfollow = (followId: string) => {
+  return axiosInstance.delete<Follow>('/follow/delete', {
+    data: { followId },
+  });
+};
