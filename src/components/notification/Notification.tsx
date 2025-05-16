a03fd3143d9bbb08cc65e9b7af400810dbb355d7
ty <composedkim@naver.com>
2025-05-16 14:19:03 +0900

    ✨Feat: 팔로우 알람 누르면 상대 프로필로 이동

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -3,10 +3,7 @@ import close from '../../assets/images/header/close.svg';
 import alarm from '../../assets/images/header/alarm.svg';
 import redDot from '../../assets/RedDotIcon.svg';
 import { useEffect, useRef, useState } from 'react';
-import {
-  getNotificationsData,
-  putNotificationSeenData,
-} from '../../api/notification/notification';
+import { getNotificationsData, putNotificationSeenData } from '../../api/notification/notification';
 import { useChannelItemStore } from '../../stores/channelStore';
 import { twMerge } from 'tailwind-merge';
 
@@ -28,11 +25,7 @@ export default function Notification() {
     let dataSum = 0;
     notifications.map((notification) => {
       // seen값이 false고 like,comment가 삭제된 내용이 아니면 카운트 되도록
-      if (
-        notification.seen === false &&
-        notification.like !== null &&
-        notification.comment !== null
-      ) {
+      if (notification.seen === false && notification.like !== null && notification.comment !== null) {
         dataSum += 1;
       }
     });
@@ -58,6 +51,13 @@ export default function Notification() {
         }
       }
     });
+
+    if (notifi.follow !== undefined && notifi.follow !== null) {
+      console.log(notifi.follow);
+      navigate('/profile', {
+        state: { userid: notifi.follow.follower },
+      });
+    }
 
     closeHandler();
   };
@@ -96,7 +96,7 @@ export default function Notification() {
   return (
     <>
       <button
-        className="relative cursor-pointer block"
+        className='relative cursor-pointer block'
         onClick={(e) => {
           e.preventDefault();
           setNotifiOpen(!notifiOpen);
@@ -104,28 +104,25 @@ export default function Notification() {
       >
         <img src={alarm} />
         {newData > 0 && (
-          <span className="block w-3.5 h-3.5 rounded-2xl bg-[#FF0000] absolute -right-1 top-0 text-[11px] text-white leading-3">
+          <span className='block w-3.5 h-3.5 rounded-2xl bg-[#FF0000] absolute -right-1 top-0 text-[11px] text-white leading-3'>
             {newData}
           </span>
         )}
       </button>
       {notifiOpen && (
-        <div className="absolute gap-3 bg-white rounded-[10px] z-1 py-4 px-5 shadow-2xl w-[340px] z-10 -right-5 top-8.5">
-          <span className="w-[12px] h-[12px] bg-white rounded-[2px] absolute rotate-135 -top-1.5 right-6 -z-2"></span>
-          <div className="border-b border-[#cccccc] flex justify-between pb-3">
-            <h3 className="text-[#4D4D4D] text-base font-medium flex items-end gap-x-2">
+        <div className='absolute gap-3 bg-white rounded-[10px] z-1 py-4 px-5 shadow-2xl w-[340px] z-10 -right-5 top-8.5'>
+          <span className='w-[12px] h-[12px] bg-white rounded-[2px] absolute rotate-135 -top-1.5 right-6 -z-2'></span>
+          <div className='border-b border-[#cccccc] flex justify-between pb-3'>
+            <h3 className='text-[#4D4D4D] text-base font-medium flex items-end gap-x-2'>
               Notifications
               {/* <span className="inline-block bg-zinc-400 rounded-3xl px-2 py-1.5 mb-0.5 min-w-7 text-center text-white text-xs leading-1.5">
                 {newData}
               </span> */}
             </h3>
           </div>
-          <div
-            ref={modalRef}
-            className="notiList px-2 h-[200px] overflow-y-auto scroll-custom relative"
-          >
+          <div ref={modalRef} className='notiList px-2 h-[200px] overflow-y-auto scroll-custom relative'>
             {notifications.length === 0 ? (
-              <p className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-sm text-[#5c5c5c]">
+              <p className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-sm text-[#5c5c5c]'>
                 알림이 없습니다
               </p>
             ) : (
@@ -133,12 +130,13 @@ export default function Notification() {
                 let thisType = '';
                 if (notifi.like !== undefined && notifi.like !== null) {
                   thisType = 'like';
-                } else if (
-                  notifi.comment !== undefined &&
-                  notifi.comment !== null
-                ) {
+                } else if (notifi.comment !== undefined && notifi.comment !== null) {
                   thisType = 'comment';
-                } else thisType = 'none';
+                } else if (notifi.follow !== undefined && notifi.follow !== null) {
+                  thisType = 'follow';
+                } else {
+                  thisType = 'none';
+                }
                 return (
                   <>
                     {thisType !== 'none' && (
@@ -146,20 +144,17 @@ export default function Notification() {
                         onClick={() => {
                           navigateHandler(notifi);
                         }}
-                        className="block relative pl-3.5 text-[13px] my-3.5 cursor-pointer text-left"
+                        className='block relative pl-3.5 text-[13px] my-3.5 cursor-pointer text-left'
                         key={`notification-${index}`}
                       >
                         <img
-                          className={twMerge(
-                            'absolute -left-0.5 top-2',
-                            notifi.seen && 'grayscale-100 opacity-30'
-                          )}
+                          className={twMerge('absolute -left-0.5 top-2', notifi.seen && 'grayscale-100 opacity-30')}
                           src={redDot}
                         />
-                        {thisType === 'like' &&
-                          `[${notifi.author['fullName']}] 님이 당신의 게시물을 좋아합니다.`}
+                        {thisType === 'like' && `[${notifi.author['fullName']}] 님이 당신의 게시물을 좋아합니다.`}
                         {thisType === 'comment' &&
                           `[${notifi.author['fullName']}] 님이 당신의 게시물에 댓글을 달았습니다.`}
+                        {thisType === 'follow' && `[${notifi.author['fullName']}] 님이 당신을 팔로우했습니다.`}
                       </button>
                     )}
                   </>
@@ -167,14 +162,14 @@ export default function Notification() {
               })
             )}
           </div>
-          <div className="absolute right-5 top-4">
+          <div className='absolute right-5 top-4'>
             <button
-              className="text-sm text-[#bbbbbb] cursor-pointer"
+              className='text-sm text-[#bbbbbb] cursor-pointer'
               onClick={() => {
                 closeHandler();
               }}
             >
-              <img src={close} className="opacity-60" />
+              <img src={close} className='opacity-60' />
             </button>
           </div>
         </div>
