ecfdc8bca782fcb6b6b572eb155fcdb249b585be
sduddla <sduddla921@gmail.com>
2025-05-18 16:02:43 +0900

    🚀 Deploy: Netlify 배포

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,18 +1,12 @@
-import axios from "axios";
-import { useAuthStore } from "../stores/authStore";
+import axios from 'axios';
+import { useAuthStore } from '../stores/authStore';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    window.location.hostname === 'localhost'
+      ? import.meta.env.VITE_API_URL
+      : '/api',
 });
-
-// export const axiosPostInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   headers: {
-//     accept: 'application/json',
-//     Authorization:
-//       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY4MWI4NjQwNDM3ZjcyMmI2OTA4YWI3MSIsImVtYWlsIjoieXViaW4xMjFAZ21haWwuY29tIn0sImlhdCI6MTc0NjYzNDQ0N30.WUUuUzDqcxT4a2XSILxX8jM8u8xch_s3hxjrQ2KGfJs',
-//   },
-// });
 
 axiosInstance.interceptors.request.use((config) => {
   const token = useAuthStore.getState().accessToken;
@@ -25,14 +19,6 @@ axiosInstance.interceptors.request.use((config) => {
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    // if (error.response?.status === 401) {
-    //   useAuthStore.getState().logout();
-    //   window.location.href = "/login";
-    // }
-
-    // if (error.response?.status === 404) {
-    //   window.location.href = '/not-found';
-    // }
     return Promise.reject(error);
   }
 );
